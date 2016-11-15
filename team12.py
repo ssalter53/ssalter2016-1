25d8639bda736df8487f68be282b4abc30194132
GerardPascual <gpascual8599@yahoo.com>
2016-11-15 10:23:21 -1000

    Revert "Name Change"
    This reverts commit ba4b36607bd41d72091e5cd13309b460e855c0b6.

--- team12.py
+++ team12.py
@@ -1,6 +1,6 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: Harambae
+#     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
