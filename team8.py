b1507568c10fbbe8e490b92c3bab802f156c0ce8
Jsapolu <jsapolu@Kapolei0.edu>
2016-11-15 10:09:58 -1000

    New team name
    We changed the team name to AppleSoft.

--- team8.py
+++ team8.py
@@ -5,8 +5,8 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+#Hey my name is Jonathan Sapolu and I was told to comment this... Ok bye.
+team_name = 'AppleSoft' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
