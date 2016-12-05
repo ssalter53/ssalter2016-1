3165919f04c8865bed499dcca0e64b43ea8d4c2c
VanAldrin <vanaldrinm@gmail.com>
2016-12-05 08:57:13 -1000

    Add files via upload

--- team7.py
+++ team7.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 7' # Only 10 chars displayed.
+strategy_name = 'KV'
+strategy_description = 'Always Betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,6 +25,7 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    
     return 'b'
 
     
