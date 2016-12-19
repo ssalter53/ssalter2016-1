4d5fef037bf581bfa0d5da3ecaffdba78f8a1c52
cibale19 <cibale@KAPOLEIHIGH.ORG>
2016-12-19 09:19:33 -1000

    Final

--- team5.py
+++ team5.py
@@ -1,12 +1,4 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'CN5' # Only 10 chars displayed.
+team_name = 'CN5' 
 strategy_name = 'Always Betray First'
 strategy_description = 'Betray if they collude?'
     
@@ -18,19 +10,12 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
     if len(their_history) < 2:
-         # If the other player has colluded within last 2 rounds,
-        return 'c' # collude.
+         return 'c' 
+    
+    elif their_history[-1] == 'b':
+            return 'b'
     return 'b'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
