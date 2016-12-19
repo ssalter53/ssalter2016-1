fb05bbb460f73e064cba670ba3328fb2600ae3c8
christianity808 <killaninja808@gmail.com>
2016-12-19 11:31:50 -1000

    random
    randomly choses B and C

--- team0.py
+++ team0.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import random
 team_name = 'Brown Town'
-strategy_name = 'B and C for x amount of sets”'
-strategy_description = 'Alternating b and c for 1 set of 10 iterations(ex.ccbbccbbcc)'
+strategy_name = 'Random'
+strategy_description = 'randomly picks C and B'
 
 def move(my_history, their_history, my_score, their_score):
     if random.randint(0,2) == 0:
